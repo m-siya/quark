2bda3386f6d8d513488d81ea9f62d9643657c690
siya <del.siyamittal@gmail.com>
2023-08-12 17:19:27 +0530

    chunk working now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+mod chunk;
+mod debug;
+mod value;
+
+use crate::chunk::{Chunk, OpCode};
+use crate::value::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let mut chunk: Chunk = Chunk::new();
+
+
+    let constant: usize = chunk.add_constant(Value::ValNumber(4.0));
+    chunk.write(OpCode::OpConstant as u8, 123);
+    chunk.write(constant as u8, 123);
+    chunk.write(OpCode::OpReturn as u8, 123);
+
+    debug::disassemble_chunk(&chunk, "test_chunk");
+
 }
